21d5e27a466938b14285bfcfa45dfff067dcb13a
Vitor <vitorrrenk@gmail.com>
2026-01-05 10:49:00 -0300

    iniciando o módulo

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodesequelize2", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (err) {
+  console.log("Unable to connect to the database: ", err);
+}
+
+module.exports = sequelize;
